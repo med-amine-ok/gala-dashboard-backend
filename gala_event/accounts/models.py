8d2dce484b417a58483807be110620fedad001cf
Your Name <your-email@example.com>
2025-10-26 22:32:35 +0100

    Add new files

--- gala_event/accounts/models.py
+++ gala_event/accounts/models.py
@@ -1,31 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Group, Permission, UserManager
+from django.contrib.auth.models import AbstractUser, Group, Permission 
 from django.utils.translation import gettext_lazy as _
 
-
-class CustomUserManager(UserManager):
-    use_in_migrations = True
-
-    def _create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError("The email must be set")
-        email = self.normalize_email(email)
-        username = extra_fields.pop('username', None) or email
-        return super()._create_user(username, email, password, **extra_fields)
-
-    def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-        return self._create_user(email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
@@ -44,8 +20,6 @@ class CustomUser(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True , blank=True)
     updated_at = models.DateTimeField(auto_now=True)
     password_set = models.BooleanField(default=False)
-
-    objects = CustomUserManager()
 
     def __str__(self):
         return f"{self.username} ({self.get_role_display()})"
