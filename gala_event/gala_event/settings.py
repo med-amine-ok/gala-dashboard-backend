d443260aacc4d21c18ed4ee8678c3e880646fdc1
Your Name <your-email@example.com>
2025-10-27 21:04:16 +0100

    Add new files

--- gala_event/gala_event/settings.py
+++ gala_event/gala_event/settings.py
@@ -98,6 +98,7 @@ CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[
     "https://mellifluous-meerkat-6b4957.netlify.app",
     "https://gala-xady.vercel.app",
     "https://vitreous-sheree-devvv-team-7eaaa976.koyeb.app",
+    "https://gala.vic-enp.com",
 ])
 CORS_ALLOW_CREDENTIALS = True
 
